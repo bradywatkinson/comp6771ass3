3475570a1beda1cbcb02f9a3d606143fafb26617
Brady Watkinson <brady.a.watkinson@unsw.edu.au>
2015-09-19 18:20:07 +1000

    Assignment Finished pending tets

--- Graph.h
+++ Graph.h
@@ -10,11 +10,185 @@
 #include <exception>
 #include <map>
 #include <set>
+#include <typeinfo>
 
-#define DEBUG true
+#define DEBUG false
 
 namespace cs6771
 {
+	template <typename N, typename E> class Graph;
+
+	template <typename N, typename E> class Node_Iterator
+	{
+		public:
+			typedef typename Graph<N,E>::Node Node;
+
+			typedef std::ptrdiff_t                     difference_type;
+			typedef std::forward_iterator_tag          iterator_category;
+			typedef N                                  value_type;
+			const typedef N*                           pointer;
+			const typedef N&                           reference;
+
+			// value type
+			reference operator*() const {
+				if (DEBUG) std::cout << "testing *: "<< (*it)->val_ << std::endl;
+				return (*it)->val_;
+			}
+			// reference type
+			pointer operator->() const {
+				return &(operator*());
+			}
+			// pre increment
+			Node_Iterator& operator++() {
+				++it;
+				if (it==mynodes_.end()) end = true;
+				return *this;
+			}
+			// post increment
+			Node_Iterator operator++(int) {
+				Node_Iterator tmp = *this;
+				++it;
+				if (it==mynodes_.end()) end = true;
+				return tmp;
+			}
+			// equals
+			bool operator==(const Node_Iterator& other) const {
+				if (DEBUG) std::cout << "testing ==: " << end << " " << other.end << std::endl;
+				if (end && other.end) {
+					return true;
+				} else if (end || other.end) {
+					return false;
+				}
+				return operator*() == (*other.it)->val_;
+			};
+			// not equals
+			bool operator!=(const Node_Iterator& other) const {
+				if (DEBUG) std::cout << "testing !=" << std::endl;
+				return !operator==(other);
+			}
+
+			//Node_Iterator(std::vector<std::shared_ptr<Node>> v)
+			Node_Iterator(const std::map<N,std::shared_ptr<Node>> *nodes = nullptr)
+			{
+				
+				if (nodes == nullptr) {
+					end = true;
+				} else {
+					//nodes_ = new std::vector<std::shared_ptr<Node>>();
+					std::for_each((*nodes).begin(),(*nodes).end(),
+						[this] (const std::pair<N,std::shared_ptr<Node>>& p) 
+							{ mynodes_.push_back(p.second); });
+
+					std::sort(mynodes_.begin(),mynodes_.end(),
+						[] (const std::shared_ptr<Node>& a, const std::shared_ptr<Node>& b) {
+							return a->numEdges_ != b->numEdges_ ?
+								a->numEdges_ > b->numEdges_ : a->val_ < b->val_;
+							});
+					it = mynodes_.begin();
+					end = false;
+				}
+			}
+
+		private:
+			bool end;
+			typename std::vector<std::shared_ptr<Node>>::iterator it;
+			typename std::vector<std::shared_ptr<Node>> mynodes_;
+
+	};
+
+	template <typename N, typename E> class Edge_Iterator
+	{
+		public:
+			friend class Graph<N,E>;
+
+			typedef typename Graph<N,E>::Node Node;
+			typedef typename Graph<N,E>::Edge Edge;
+
+			typedef std::ptrdiff_t                     difference_type;
+			typedef std::forward_iterator_tag          iterator_category;
+			typedef E                                  value_type;
+			const typedef E*                           pointer;
+			const typedef E&                           reference;
+
+			// value type
+			reference operator*() const {
+				if (DEBUG) std::cout << "testing *: "<< (*it)->val_ << std::endl;
+				return (*it)->val_;
+			}
+			// reference type
+			pointer operator->() const {
+				return &(operator*());
+			}
+			// pre increment
+			Edge_Iterator& operator++() {
+				++it;
+				if (it==myedges_.end()) end = true;
+				return *this;
+			}
+			// post increment
+			Edge_Iterator operator++(int) {
+				Edge_Iterator tmp = *this;
+				++it;
+				if (it==myedges_.end()) end = true;
+				return tmp;
+			}
+			// equals
+			bool operator==(const Edge_Iterator& other) const {
+				if (DEBUG) std::cout << "testing ==: " << end << " " << other.end << std::endl;
+				if (end && other.end) {
+					return true;
+				} else if (end || other.end) {
+					return false;
+				}
+				return operator*() == (*other.it)->val_;
+			};
+			// not equals
+			bool operator!=(const Edge_Iterator& other) const {
+				if (DEBUG) std::cout << "testing !=" << std::endl;
+				return !operator==(other);
+			}
+
+			//Edge_Iterator(std::vector<std::shared_ptr<Edge>> v)
+			Edge_Iterator(const std::shared_ptr<Node> node = nullptr)
+			{
+				if (node == nullptr || node->edges_.empty()) {
+					end = true;
+				} else {
+					std::for_each(node->edges_.begin(),node->edges_.end(),
+						[this] (const std::shared_ptr<Edge>& e) 
+							{ myedges_.push_back(e); });				
+
+					std::sort(myedges_.begin(),myedges_.end(),
+						[] (const std::shared_ptr<Edge>& a, const std::shared_ptr<Edge>& b) {
+							if (a->val_ == b->val_) {
+								if (auto tmpa = a->dest.lock()) {
+									if (auto tmpb = b->dest.lock()) {
+										return tmpa->val_ < tmpb->val_;
+									}
+								}
+							}
+							return a->val_ < b->val_;
+						});
+					it = myedges_.begin();
+					end = false;
+				}
+			}
+
+		private:
+			N getDest () {
+				if (auto tmp = (*it)->dest.lock()) {
+					return tmp->val_;
+				}
+				throw std::runtime_error("getDest: Node DNE");
+			}
+
+			bool end;
+			typename std::vector<std::shared_ptr<Edge>>::iterator it;
+			typename std::vector<std::shared_ptr<Edge>> myedges_;
+
+	};
+
+
 	template <typename N, typename E>
 	class Graph
 	{
@@ -24,76 +198,60 @@ namespace cs6771
 
 			struct Node
 			{
-				N val;
+				N val_;
+				int numEdges_;
 				std::set<std::shared_ptr<Edge>> edges_;
 				Node() {}
-				Node(const N x) : val{x} {}
+				Node(const N x) : val_{x} {numEdges_=0;}
 				void printNode (N n) {
-					if (n!=val) throw std::runtime_error("Node corruption: index != val");
-					std::cout << val << ": ";
+					if (n!=val_) throw std::runtime_error("Node corruption: index != val_");
+					std::cout << val_ << ": ";
 					for (std::shared_ptr<Edge> e : edges_) (*e).printEdge();
 					std::cout << std::endl;
 				} 
-				// Node& makeNode (N val) {
-				// 	return Node{val};
-				// }
-				// Node& operator=(const Node &n)
-				// {
-				// 	val = n.val;
-				// 	edges_ = n.edges_;
-				// 	return *this;
-				// }
-				//bool operator<(const Node& n) const {return val<n.val;}
-				//static bool nodeCome(const Node& lhs, const Node& rhs) {return lhs.val<rhs.val;}
 			};
 
 			struct Edge
 			{
 				std::weak_ptr<Node> orig;
 				std::weak_ptr<Node> dest;
-				E val;
-				Edge(std::shared_ptr<Node> o, std::shared_ptr<Node> d, E x) : val{x} {//orig{o}, dest{d}, 
-					//orig = std::make_shared<Node>(o);
-					//dest = std::make_shared<Node>(d);
+				E val_;
+				Edge(std::shared_ptr<Node> o, std::shared_ptr<Node> d, E x) : val_{x} {//orig{o}, dest{d}, 
 					orig = o;
 					dest = d;
 				}
 				Edge() {};
 				void printEdge () {
-					//auto tmp = std::make_shared<Node>(dest);
 					if (auto tmp = dest.lock())
-						std::cout << "[" << val << "] " << tmp->val << " ";
+						std::cout << "[" << val_ << "] " << tmp->val_ << " ";
 				}
 			};
 
 		public:
 
+			friend class Node_Iterator<N,E>;
+			typedef Node_Iterator<N,E> Node_Iterator;
+			
+			friend class Edge_Iterator<N,E>;
+			typedef Edge_Iterator<N,E> Edge_Iterator;
+			
 			Graph() :
 				nodes_{}
-				//edges_{},
-				//graph_{}
 				{};
 
 			//copy constructor
 			Graph(const Graph &g) :
 				nodes_{g.nodes_}
-				// edges_{g.edges_},
-				// graph_{g.graph_}
 				{};
 
 			//move constructor
 			Graph(Graph &&g) :
 				nodes_{std::move(g.nodes_)}
-				// edges_{std::move(g.edges_)},
-				// graph_{std::move(g.graph_)}
 				{};
 			
 			//Destructor
 			~Graph()
 			{
-				//delete nodes_;
-				//delete edges_;
-				//delete graph_;
 			};
 
 			//=copy Operator
@@ -101,8 +259,6 @@ namespace cs6771
 			{
 				if (this != g) {
 					nodes_ = g.nodes_;
-					// edges_ = g.edges_;
-					// graph_ = g.graph_;
 				}
 				return *this;
 			}
@@ -112,45 +268,22 @@ namespace cs6771
 			{
 				if (this != g) {
 					nodes_ = std::move(g.nodes_);
-					// edges_ = std::move(g.edges_);
-					// graph_ = std::move(g.graph_);
 				}
 				return *this;
 			}
 
-			bool addNode(const N& val)
+			bool addNode(const N& val_)
 			{
-				if (nodes_.find(val) == nodes_.end()) {
-					if (DEBUG) std::cout << "adding: " << val << std::endl;
-					nodes_[val] = std::make_shared<Node>(Node{val});
+				if (nodes_.find(val_) == nodes_.end()) {
+					if (DEBUG) std::cout << "adding: " << val_ << std::endl;
+					nodes_[val_] = std::make_shared<Node>(Node{val_});
 				} else if (DEBUG) {
-					std::cout << val << ": not added. Already exists" << std::endl;
+					std::cout << val_ << ": not added. Already exists" << std::endl;
 				}
 
 				return true;
 			}
 
-			// bool addNode(const N& n)
-			// {
-			// 	auto result = std::find_if(nodes_.begin(),nodes_.end(),
-			// 				[n] (const Node node) {return node.val == n;});
-
-			// 	if (result == nodes_.end()) {
-			// 		if (DEBUG) std::cout << "adding: " << n << std::endl;
-			// 		Node tmp{n};
-			// 		nodes_.insert(tmp);
-			// 		return true;
-			// 	} else if (DEBUG) {
-			// 		std::cout << n << ": not added. Already exists" << std::endl;
-			// 	}
-			// 	return true;
-			// }
-/*
-A method to add an Edge between two nodes. The method will be passed the origin and
-destination nodes and the weight object. The method returns true if the edge is added,
-false if there is already an edge between the two nodes with the same weight object.
-A std::runtime_error is thrown if either the origin or destination node is not found in the graph.
-*/
 			bool addEdge(const N& orig, const N& dest, const E& val)
 			{
 				// check that both orig and dest exist
@@ -161,13 +294,22 @@ A std::runtime_error is thrown if either the origin or destination node is not f
 				//if (findDest == nodes_.end()) std::runtime_error("cannot add edge from"+orig+" to "+dest+", "+dest+" does not exist");
 
 				// check if the edge between already exists
+				// auto findEdge = std::find_if(findOrig->second->edges_.begin(),findOrig->second->edges_.end(),
+				// 							[val_] (const std::shared_ptr<Edge>& e) {return e->val_==val_;});
+
 				auto findEdge = std::find_if(findOrig->second->edges_.begin(),findOrig->second->edges_.end(),
-											[val] (std::shared_ptr<Edge> e) {return e->val==val;});
+											[val,dest] (const std::shared_ptr<Edge>& e) {
+												if (e->val_ == val)
+													if (auto tmp = e->dest.lock())
+														if (tmp->val_ == dest)
+															return true;
+												return false;
+											});
 
 				if (findEdge == findOrig->second->edges_.end()) {
 					if (DEBUG) std::cout << "adding edge: " << orig << " " << dest << " " << val << std::endl;
-					
 					findOrig->second->edges_.insert(std::make_shared<Edge>(Edge(findOrig->second,findDest->second,val)));
+					++findOrig->second->numEdges_;
 					return true;
 				} else if (DEBUG) {
 					std::cout << orig << "->" << dest << " not added. Already exists" << std::endl;
@@ -175,55 +317,27 @@ A std::runtime_error is thrown if either the origin or destination node is not f
 				return false;
 			}
 
-			// bool addEdge(const N& orig, const N& dest, const E& edge)
-			// {
-			// 	auto result1 = find_if(nodes_.begin(),nodes_.end(),
-			// 				[orig] (const Node n) {return n.val==orig;});
-			// 	auto result2 = find_if(nodes_.begin(),nodes_.end(),
-			// 				[dest] (const Node n) {return n.val==dest;});
-
-			// 	if (result1 == nodes_.end()) std::runtime_error("cannot add edge from"+orig+" to "+dest+", "+orig+" does not exist");
-			// 	if (result2 == nodes_.end()) std::runtime_error("cannot add edge from"+orig+" to "+dest+", "+dest+" does not exist");
-
-			// 	if (result1->edges_)
-
-			// 	return true;
-			// }	
-
-/*
-Replaces the data stored at a node. If the current node cannot be found a std::runtime_error
-is thrown. If the replacement node data already exists on a different node in the graph the
-method returns false. Otherwise return true.
-*/
 			bool replace(const N& node, const N& val)
 			{
 				// check the node exists
 				auto findNode = nodes_.find(node);
 				if (findNode == nodes_.end()) throw std::runtime_error("replace: Node DNE");
 
-				// check if the new val exists
-				auto findVal = nodes_.find(val);
-				// auto findVal = std::find_if(nodes_.begin(),nodes_.end(),
-				//					[val] (std::pair<N,std::shared_ptr<Node>> n) {return n.second->val == val;});
+				// check if the new val_ exists
+				auto findval_ = nodes_.find(val);
 
-				//if (nodes_[val] == NULL) {
-
-				if (findVal == nodes_.end()) {
-					if (DEBUG) std::cout << "replacing " << findNode->second->val << " with " << val << std::endl;
+				if (findval_ == nodes_.end()) {
+					if (DEBUG) std::cout << "replacing " << findNode->second->val_ << " with " << val << std::endl;
 					nodes_[val] = nodes_[node];
-					nodes_[val]->val = val;
+					nodes_[val]->val_ = val;
 					nodes_.erase(findNode);
-					//findNode->second->val = 66;	
+					//findNode->second->val_ = 66;	
 					return false;
 				}
 				if (DEBUG) std::cout << "replace: " <<val << " already exists" << std::endl;
 				return true;
 			} 
-/*
-;	Deletes a given node and all edges connected to and from it.
-Hint: if you are using weak ptrs for edges you may be able to do this
-quite simply. This method should not throw any exceptions.
-*/			
+
 			void deleteNode(const N& node) noexcept
 			{
 				auto findNode = nodes_.find(node);
@@ -235,25 +349,24 @@ quite simply. This method should not throw any exceptions.
 					if (DEBUG) std::cout << "could not find " << node << std::endl;
 				}
 			}
-/*
-Deletes an edge between two nodes with a given weight. This method
-should not throw any exceptions. If the given edge is not found do nothing.
-*/
+
 			void deleteEdge(const N& orig, const N& dest, const E& val) noexcept
 			{
 				// find the origin node
 				auto findNode = nodes_.find(orig);
-				// find the destination node: Must have same dest AND val
+				// find the destination node: Must have same dest AND val_
 				if (findNode != nodes_.end()) {
 					auto findEdge = std::find_if(findNode->second->edges_.begin(), findNode->second->edges_.end(),
-											[dest, val] (std::shared_ptr<Edge> e) {
-												if (auto tmp = e->dest.lock())
-													if (tmp->val == dest && e->val == val)
-														return true;
+											[dest, val] (const std::shared_ptr<Edge>& e) {
+												if (e->val_ == val)
+													if (auto tmp = e->dest.lock())
+														if (tmp->val_ == dest)
+															return true;
 												return false;
 											});
 					if (findEdge != findNode->second->edges_.end()) {
 						findNode->second->edges_.erase(findEdge);
+						++findNode->second->numEdges_;
 					} else if (DEBUG) {
 						std::cout << "deleteEdge: no edge was found" << std::endl;
 					}
@@ -262,54 +375,91 @@ should not throw any exceptions. If the given edge is not found do nothing.
 				}
 			}
 
-/*
-Removes all nodes and edges from the graph. New nodes and edges can
-be added to the graph after the clear command.
-*/
-
 			void clear() noexcept
 			{
 				nodes_.clear();
 			}
-/*
-Returns true if the data value passed into the function is
-already stored as a node in the graph.
-*/
+
 			bool isNode(const N& node) const
 			{
 				return nodes_.find(node) != nodes_.end();
 			}
 
-/*
-Returns true if there is an edge between two nodes, false
-otherwise. Throws a std::runtime_error if there is either
-node is not in the graph.
-*/
 			bool isConnected(const N& orig, const N& dest) const
 			{
-				// auto findOrig = nodes_.find(orig);
-				// if (findOrig == nodes_.end()) throw std::runtime_error("addEdge: origin DNE");
-				// auto findDest = nodes_.find(dest);
-				// if (findDest == nodes_.end()) throw std::runtime_error("addEdge: dest DNE");
+				auto findOrig = nodes_.find(orig);
+				if (findOrig == nodes_.end()) throw std::runtime_error("addEdge: origin DNE");
+				auto findDest = nodes_.find(dest);
+				if (findDest == nodes_.end()) throw std::runtime_error("addEdge: dest DNE");
 
+				auto findEdge = std::find_if(findOrig->second->edges_.begin(), findOrig->second->edges_.end(),
+											[dest] (const std::shared_ptr<Edge>& e) {
+												if (auto tmp = e->dest.lock())
+													if (tmp->val_ == dest)
+														return true;
+												return false;
+											});
+				return findEdge != findOrig->second->edges_.end()? true : false;
+			}
 
+			/*
+			 * Prints the nodes in descending number of edges order
+			 */
+			void printNodes()
+			{
+				for (auto it = begin(); it != end(); ++it ) {
+					std::cout << *it << std::endl;
+				}
+			}
+
+			void printEdges(const N& node) const
+			{
+				std::cout << "Edges attached to Node " << node << std::endl;
+				bool tmp = false;
+				for (auto it = edgeIteratorBegin(node); it != edgeIteratorEnd(node); ++it) {
+					std::cout << it.getDest() << " " << *it << std::endl;
+					tmp = true;
+				}
+				if (!tmp) std::cout << "(null)" << std::endl;
 			}
 
 			void printGraph ()
 			{
 				std::cout << "Printing Graph" << std::endl;
-				for (std::pair<N,std::shared_ptr<Node>> n : nodes_) {
+				for (auto n : nodes_) {
 					n.second->printNode(n.first);
 				}
 			}
 
-		private:
-			//std::set<Node> nodes_;
-			//std::vector<std::shared_ptr<Node>> nodes_;
-			//std::vector<std::shared_ptr<Edge>> edges_;
+			Node_Iterator begin() const
+			{
 
+				return Node_Iterator(&nodes_);
+			};
+
+
+			Node_Iterator end() const
+			{
+				return Node_Iterator(nullptr);
+			}
+
+			Edge_Iterator edgeIteratorBegin(const N& node) const
+			{
+				auto findNode = nodes_.find(node);
+				if (findNode != nodes_.end()) {
+					//if (findNode->second)
+					return Edge_Iterator(findNode->second);
+				}
+				throw std::runtime_error("edgeIteratorBegin: edge DNE");
+			}
+
+			Edge_Iterator edgeIteratorEnd (const N& node) const 
+			{
+				return Edge_Iterator(nullptr);
+			}
+
+		private:
 			std::map< N, std::shared_ptr<Node> > nodes_;
-			//std::vector< std::vector<*Node> > graph;
 	};
 }
 
